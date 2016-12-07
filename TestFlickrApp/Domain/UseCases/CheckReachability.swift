b878e7d7ed5a104bb21a0091c74038d813a143e5
takumi314 <isospin_symmetry@me.com>
2016-12-08 02:14:47 +0900

    Add a method to check if the given target is reachable on the network

--- TestFlickrApp/Domain/UseCases/CheckReachability.swift
+++ TestFlickrApp/Domain/UseCases/CheckReachability.swift
@@ -8,3 +8,23 @@
 
 import SystemConfiguration
 
+class CheckReachability {
+    
+    // MARK: - デバイスがオンラインかどうかを判定する
+    func checkReachability(hostName: String) -> Bool {
+        
+        let reachability = SCNetworkReachabilityCreateWithName(nil, hostName)!
+        var flags = SCNetworkReachabilityFlags.ConnectionAutomatic
+        
+        //Determines if the given target is reachable using the current network configuration.
+        guard SCNetworkReachabilityGetFlags(reachability, &flags) else {
+            return false
+        }
+        
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        return (isReachable && !needsConnection)
+    }
+    
+}
