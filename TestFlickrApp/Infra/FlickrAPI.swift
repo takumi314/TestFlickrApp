23e1c16916f93198c04e65671aa6d6eba5dd2453
takumi314 <isospin_symmetry@me.com>
2016-12-16 01:28:19 +0900

    Add FlickrAPI

---
+++ TestFlickrApp/Infra/FlickrAPI.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrAPI.swift
+//  TestFlickrApp
+//
+//  Created by NishiokaKohei on 2016/12/16.
+//  Copyright © 2016年 Kohey. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Flickr: AnyObject {
+    
+}
